ee9f2071008cfe0e0721e505eb50a6a5bca0685b
Kubilay Bilgili <kubilay.bilgili@gmail.com>
2021-09-15 01:49:29 +0300

    + Chapter 6 Exercises 10-20

---
+++ src/chapter6/methods/exercises/Exercise_6_17.java
@@ -0,0 +1,49 @@
+/*
+(Display matrix of 0s and 1s) Write a method that displays an n-by-n matrix
+using the following header: Here is a sample run:
+public static int printMatrix(int year)
+that prompts the user to enter n and displays an n-by-n matrix.
+ */
+package chapter6.methods.exercises;
+
+
+import java.util.Scanner;
+
+public class Exercise_6_17 {
+    public static void main(String[] args) {
+
+        Scanner inout = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n = inout.nextInt();
+        printMatrix(n);
+    }
+    public static void printMatrix(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                byte randomBit = (byte) (Math.random() * 2);
+                if (j == n-1)
+                    System.out.print(randomBit + " \n");
+                else
+                    System.out.print(randomBit + " ");
+
+            }
+        }
+    }
+
+}
+
+/* OUTPUT
+2000 366 days
+2001 365 days
+2002 365 days
+2003 365 days
+2004 366 days
+...
+2014 365 days
+2015 365 days
+2016 366 days
+2017 365 days
+2018 365 days
+2019 365 days
+2020 366 days
+*/
---
+++ src/chapter6/methods/exercises/Exercise_6_21.java
@@ -0,0 +1,26 @@
+/*
+(Phone keypads) The international standard letter/number mapping for telephones
+is given in Programming Exercise 4.15. Write a method that returns a
+number, given an uppercase letter, as follows:
+public static int getNumber(char uppercaseLetter)
+Write a test program that prompts the user to enter a phone number as a string.
+The input number may contain letters. The program translates a letter (uppercase
+or lowercase) to a digit and leaves all other characters intact.
+ */
+package chapter6.methods.exercises;
+
+
+import java.util.Scanner;
+
+public class Exercise_6_21 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+    }
+
+}
+
+/* OUTPUT
+
+*/
