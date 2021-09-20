3c3a2aa6cf72e7868c44c3ad5e494e6a17e3a08d
Kubilay Bilgili <kubilay.bilgili@gmail.com>
2021-09-20 16:19:39 +0300

    + Chapter 8 Listings

---
+++ src/chapter8/array2d/listings/GradeExam.java
@@ -0,0 +1,37 @@
+/* Listing 8.2 */
+package chapter8.array2d.listings;
+
+import java.util.Scanner;
+
+public class GradeExam {
+    public static void main(String[] args) {
+        char[] keys = {'A', 'B', 'A', 'C', 'D'};
+        char[][] answers = {
+                {'A', 'C', 'A', 'B', 'D'},
+                {'B', 'C', 'A', 'A', 'D'},
+                {'A', 'D', 'B', 'A', 'D'},
+                {'C', 'C', 'B', 'A', 'D'},
+                {'A', 'B', 'A', 'C', 'D'},
+                {'A', 'C', 'C', 'A', 'D'}};
+
+        int highestCount = 0;
+        int highestCountStudent = 0;
+        for (int student = 0; student < answers.length; student++) {
+            int correctCount = 0;
+            for (int answer = 0; answer < answers[student].length; answer++) {
+                if (answers[student][answer] == keys[answer])
+                    correctCount++;
+            }
+            if (correctCount > highestCount) {
+                highestCount = correctCount;
+                highestCountStudent = student;
+            }
+            System.out.println("Student " + student + "'s correct count is " + correctCount);
+
+
+        }
+        System.out.println("Highest count is: " + highestCount + " student is " + highestCountStudent);
+
+
+    }
+}
