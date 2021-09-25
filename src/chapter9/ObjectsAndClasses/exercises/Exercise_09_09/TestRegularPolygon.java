cccd9e28575d8db7258f787a570487fae04a6755
Kubilay Bilgili <kubilay.bilgili@gmail.com>
2021-09-25 10:44:10 +0300

    + Added Chapter 9 Exercise 9

--- src/chapter9/ObjectsAndClasses/exercises/Exercise_09_09/TestRegularPolygon.java
+++ src/chapter9/ObjectsAndClasses/exercises/Exercise_09_09/TestRegularPolygon.java
@@ -28,67 +28,22 @@ using RegularPolygon(6, 4), and using RegularPolygon(10, 4,
  */
 package chapter9.ObjectsAndClasses.exercises.Exercise_09_09;
 
-public class RegularPolygon {
-    private int n; // number of sides
-    private double side; // side length
-    private double x; // x coordinate
-    private double y; // y coordinate
+public class TestRegularPolygon {
+    public static void main(String[] args) {
 
-    public RegularPolygon() {
-        this(3, 1);
-    }
+        RegularPolygon polygon1 = new RegularPolygon(6, 4);
+        RegularPolygon polygon2 = new RegularPolygon(10, 4, 5.6, 7.8);
 
-    public RegularPolygon(int n, double side) {
-        this.n = n;
-        this.side = side;
-        this.x = 0;
-        this.y = 0;
-    }
-
-    public RegularPolygon(int n, double side, double x, double y) {
-        this.n = n;
-        this.side = side;
-        this.x = 0;
-        this.y = 0;
-    }
-
-    public int getN() {
-        return n;
-    }
-
-    public void setN(int n) {
-        this.n = n;
-    }
-
-    public double getSide() {
-        return side;
-    }
-
-    public void setSide(double side) {
-        this.side = side;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    public double getArea() {
-        return (n * side * side) / (4 * Math.tan(Math.PI / n));
-    }
-
-    public double getPerimeter() {
-        return n * side;
+        System.out.println("Polygon1 Perimeter: " + polygon1.getPerimeter());
+        System.out.println("Polygon1 Area: " + polygon1.getArea());
+        System.out.println("Polygon2 Perimeter: " + polygon2.getPerimeter());
+        System.out.println("Polygon2 Area: " + polygon2.getArea());
     }
 }
+
+/* OUTPUT
+Polygon1 Perimeter: 24.0
+Polygon1 Area: 41.569219381653056
+Polygon2 Perimeter: 40.0
+Polygon2 Area: 123.10734148701015
+ */
